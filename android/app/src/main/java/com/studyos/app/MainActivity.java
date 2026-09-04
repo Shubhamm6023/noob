6dbb94d66c5aec4a8a5b96140e1329b9a8649b53
shubhamm6023 <shubhamrao0807@gmail.com>
2026-09-03 22:44:47 -0700

    Improve Android phone layout and hacker icon

--- android/app/src/main/java/com/studyos/app/MainActivity.java
+++ android/app/src/main/java/com/studyos/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.graphics.Color;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     private WebView webView;
@@ -12,12 +14,20 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.rgb(7, 11, 9));
+        window.setNavigationBarColor(Color.rgb(7, 11, 9));
+        window.getDecorView().setSystemUiVisibility(0);
         webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(7, 11, 9));
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         webView.setWebViewClient(new WebViewClient());
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
+        settings.setLoadWithOverviewMode(false);
+        settings.setUseWideViewPort(false);
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
